6cb3c9c56ccfa88567194fef524367ee24a8face
OMPHEMETSE <neodhlamini3@gmail.com>
2025-12-01 19:23:57 +0200

    Initial Commit

---
+++ Clean-Flow app.v1/Clean_Flow_app_v1App.swift
@@ -0,0 +1,17 @@
+//
+//  Clean_Flow_app_v1App.swift
+//  Clean-Flow app.v1
+//
+//  Created by Omphemetse on 2025/12/01.
+//
+
+import SwiftUI
+
+@main
+struct Clean_Flow_app_v1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Clean-Flow app.v1Tests/Clean_Flow_app_v1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Clean_Flow_app_v1Tests.swift
+//  Clean-Flow app.v1Tests
+//
+//  Created by Omphemetse on 2025/12/01.
+//
+
+import Testing
+@testable import Clean_Flow_app_v1
+
+struct Clean_Flow_app_v1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
